5d266ed96a1450411996fb0ec6e1662b17ea72b4
NioTex92 <66276727+NioTex92@users.noreply.github.com>
2020-07-08 01:54:25 +0200

    Delete java.js

--- css/java.js
+++
@@ -1,6 +0,0 @@
-function calcAmount() {
-    let price = 1000;
-    let amountinput = document.querySelector("input[name='amount-input']");
-    let amount = parseInt(amountinput.value) * price;
-    alert("Fizetendő: " + amount + "FT");
-}
