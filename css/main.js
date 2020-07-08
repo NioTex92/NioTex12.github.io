0e333af5ae60858785be2351a0d2d6d8b2134541
NioTex92 <jeno.mircse@gmail.com>
2020-07-08 19:50:28 +0200

    2020.07.08.19:50 - java

--- css/main.js
+++ css/main.js
@@ -1,15 +1,18 @@
 function calcAmount() {
-let price = 1200;
-let amountInput = document.querySelector("input[name='amount-input']");
-let amount = parseInt(amountInput.value) * price
-alert("FIzetendő: " + amount + "Ft")
- }
+   let price = 1000;
+   let amountInput = document.querySelector("input[name='amount-input']");
+   let showAmount = document.querySelector("span.show-amount");
+   let amountNumber = parseInt(amountInput.value);
 
- function calcAmount() {
-    let price = 1200;
-    let amountInput = document.querySelector("input[name='amount-input']");
-    let showAmount = document.querySelector("span.show-amount");
-    let amount = parseInt(amountInput.value) * price
+   if ( amountNumber > 10 ) {
+      alert("Maximum 10 termék");
+      return;
+   } else if ( amountNumber < 1 ) {
+      alert("Minimum 1 termék");
+   } else {
+      let amount = parseInt(amountInput.value) * price;
+      showAmount.innerHTML = amount;
+   }
 
-    showAmount.innerHTML = amount;
-     }
+
+}
