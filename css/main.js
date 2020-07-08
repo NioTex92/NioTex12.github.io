166aa736e0a2f3ff7fa29a7aeae5ade042b68276
NioTex92 <jeno.mircse@gmail.com>
2020-07-08 18:53:21 +0200

    f

--- css/main.js
+++ css/main.js
@@ -4,3 +4,12 @@ let amountInput = document.querySelector("input[name='amount-input']");
 let amount = parseInt(amountInput.value) * price
 alert("FIzetendő: " + amount + "Ft")
  }
+
+ function calcAmount() {
+    let price = 1200;
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let showAmount = document.querySelector("span.show-amount");
+    let amount = parseInt(amountInput.value) * price
+
+    showAmount.innerHTML = amount;
+     }
