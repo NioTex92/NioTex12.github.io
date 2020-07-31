f9b5c9cf1c9b11c9e271b33f950c45e4fd9a4bcb
NioTex92 <66276727+NioTex92@users.noreply.github.com>
2020-08-01 01:58:36 +0200

    Flex 2020.08.01
    DOM

---
+++ js/main2F.js
@@ -0,0 +1,32 @@
+function calcAmount() {
+    let price = 1000;
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let amountNumber = parseInt(amountInput.value);
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+    
+    showSumPrice(price, amountNumber);
+    // függvény lehivása
+   }
+   
+  // szétválasztva
+   function showSumPrice (price, amountNumber) {
+      let showAmount = document.querySelector("span.show-amount");
+      if ( amountNumber > 10 ) {
+         alert("Maximum 10 termék");
+         return;
+      } else if ( amountNumber < 1 ) {
+         alert("Minimum 1 termék");
+      } else {
+         let amount = amountNumber * price;
+         showAmount.innerHTML = amount;
+   }
+   
+
+}
+
+// Űrlap események
+let orderForm = document.querySelector("#orderForm");
+orderForm.addEventListener("submit", function(ev) {
+   ev.preventDefault();
+   console.log(this);
+});
