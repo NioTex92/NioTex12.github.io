35edcb46e73f172948394268d85fc79636f10cdc
NioTex92 <66276727+NioTex92@users.noreply.github.com>
2020-08-01 02:12:44 +0200

    Update main2F.js
    v2,3

--- js/main2F.js
+++ js/main2F.js
@@ -28,11 +28,12 @@ function calcAmount() {
 let orderForm = document.querySelector("#orderForm");
 orderForm.addEventListener("submit", function(ev) {
    ev.preventDefault();
-
+   console.log(this);
+    
    let inputs = this.querySelectorAll("input");
    let values = {};
    for (let i = 0; i < inputs.length; i++) {
       values[inputs[i].name] = inputs[i].value;
    }
-   console.log ( values );
+   console.log(values);
 });
