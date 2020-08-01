a41ab29c900204c87eef00117e4053d95a7117ca
NioTex92 <66276727+NioTex92@users.noreply.github.com>
2020-08-01 03:33:15 +0200

    Add files via upload

--- js/main2F.js
+++ js/main2F.js
@@ -1,35 +1,35 @@
 function calcAmount() {
-    let price = 1000;
-    let amountInput = document.querySelector("input[name='amount-input']");
-    let amountNumber = parseInt(amountInput.value);
-    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
-    
-    showSumPrice(price, amountNumber);
-    // függvény lehivása
+   let price = 1000;
+   let amountInput = document.querySelector("input[name='amount-input']");
+   let amountNumber = parseInt(amountInput.value);
+   amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+
+   showSumPrice(price, amountNumber);
+   // függvény lehivása
+}
+
+// szétválasztva
+function showSumPrice(price, amountNumber) {
+   let showAmount = document.querySelector("span.show-amount");
+   if (amountNumber > 10) {
+      alert("Maximum 10 termék");
+      return;
+   } else if (amountNumber < 1) {
+      alert("Minimum 1 termék");
+   } else {
+      let amount = amountNumber * price;
+      showAmount.innerHTML = amount;
    }
-   
-  // szétválasztva
-   function showSumPrice (price, amountNumber) {
-      let showAmount = document.querySelector("span.show-amount");
-      if ( amountNumber > 10 ) {
-         alert("Maximum 10 termék");
-         return;
-      } else if ( amountNumber < 1 ) {
-         alert("Minimum 1 termék");
-      } else {
-         let amount = amountNumber * price;
-         showAmount.innerHTML = amount;
-   }
-   
+
 
 }
 
 // Űrlap események
 let orderForm = document.querySelector("#orderForm");
-orderForm.addEventListener("submit", function(ev) {
+orderForm.addEventListener("submit", function (ev) {
    ev.preventDefault();
    console.log(this);
-    
+
    let inputs = this.querySelectorAll("input");
    let values = {};
    for (let i = 0; i < inputs.length; i++) {
@@ -38,28 +38,10 @@ orderForm.addEventListener("submit", function(ev) {
    console.log(values);
 });
 
-function calcAmount() {
-    let price = 1000;
-    let amountInput = document.querySelector("input[name='amount-input']");
-    let amountNumber = parseInt(amountInput.value);
-    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
-    
-    showSumPrice(price, amountNumber);
-    // függvény lehivása
-   }
-   
-  // szétválasztva
-   function showSumPrice (price, amountNumber) {
-      let showAmount = document.querySelector("span.show-amount");
-      if ( amountNumber > 10 ) {
-         alert("Maximum 10 termék");
-         return;
-      } else if ( amountNumber < 1 ) {
-         alert("Minimum 1 termék");
-      } else {
-         let amount = amountNumber * price;
-         showAmount.innerHTML = amount;
-   }
-   
-
+// Parent element kezelése
+let alertCloseButtons = document.querySelectorAll(".cloese[data-dismiss='alert']")
+for (let i = 0; i < alertCloseButtons.length; i++) {
+   alertCloseButtons[i].addEventListener("click", function (ev) {
+      console.log(ev)
+   });
 }
