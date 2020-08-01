3704899a1c365fa2850adc9891faa5c5907e10be
NioTex92 <66276727+NioTex92@users.noreply.github.com>
2020-08-01 02:06:50 +0200

    Add files via upload
    v2

--- js/main2F.js
+++ js/main2F.js
@@ -29,4 +29,11 @@ let orderForm = document.querySelector("#orderForm");
 orderForm.addEventListener("submit", function(ev) {
    ev.preventDefault();
    console.log(this);
+
+   let inputs = this.querySelectorAll("input")
+   let values = {};
+   for (let i = 0; i < inputs.length; i++) {
+      values[inputs[i].name] = inputs[i].value;
+   }
+   console.log ( values );
 });
